df003deb06eea4deea5a12407d792aa6e198de49
devaanshmann <devaanshmann5@gmail.com>
2025-09-12 13:57:16 -0700

    Inheritance Package

---
+++ src/Inheritance/HourlyEmployee.java
@@ -0,0 +1,29 @@
+package Inheritance;
+
+class HourlyEmployee extends Employee {
+
+    private double wage;
+    private double hoursWorked;
+
+    public HourlyEmployee(String fName, String lName, String SSN, double wage, double hoursWorked) {
+        super(fName, lName, SSN);
+        this.wage = wage;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+}
