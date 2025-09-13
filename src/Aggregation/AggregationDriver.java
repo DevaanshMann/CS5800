146dfb7a59c5da5ce915a727502ba012b7777d6a
devaanshmann <devaanshmann5@gmail.com>
2025-09-12 17:30:16 -0700

    Agrregation Package

--- src/Aggregation/AggregationDriver.java
+++ src/Aggregation/AggregationDriver.java
@@ -1,4 +1,36 @@
 package Aggregation;
 
 public class AggregationDriver {
+    public static void main(String[] args) {
+
+        Instructor inst1 = new Instructor("Nima", "Davarpanah", "3-2636");
+        Instructor inst2 = new Instructor("Jane", "Doe", "1-2345");
+
+        Textbook tBook1 = new Textbook("Clean Code", "Robert C. Martin", "Prentice Hall");
+        Textbook tBook2 = new Textbook("Design Patterns", "Erich Gamma", "Addison-Wesley");
+
+        Course cs5800 = new Course("CS 5800");
+
+        // used add methods to add both instructors and textbooks
+        cs5800.addInstructor(inst1);
+        cs5800.addInstructor(inst2);
+
+        cs5800.addTextbook(tBook1);
+        cs5800.addTextbook(tBook2);
+
+        cs5800.print();
+    }
 }
+
+
+//public class AggregationDriver {
+//    public static void main(String[] args) {
+//
+//        Instructor inst1 = new Instructor("Nima", "Davarpanah", "3-2636");
+//        Textbook tBook1 = new Textbook("Clean Code", "Robert C. Martin", "Prentice Hall");
+//        Course cs5800 =  new Course("CS5800", inst1, tBook1);
+//        cs5800.print();
+//
+//        System.out.println();
+//    }
+//}
--- src/Aggregation/Course.java
+++ src/Aggregation/Course.java
@@ -1,14 +1,16 @@
 package Aggregation;
 
+import java.util.ArrayList;
+
 public class Course {
     private String cName;
-    private Instructor inst;            // can string be used?
-    private Textbook tbook;
+    private ArrayList<Instructor> Inst;
+    private ArrayList<Textbook> tBook;
 
-    public Course(String courseName, Instructor instructor, Textbook textbook) {
+    public Course(String courseName) {
         this.cName = courseName;
-        this.inst = instructor;
-        this.tbook = textbook;
+        this.Inst = new ArrayList<>();
+        this.tBook = new ArrayList<>();
     }
 
     public String getCourseName() {
@@ -17,22 +19,49 @@ public class Course {
     public void setCourseName(String courseName) {
         this.cName = courseName;
     }
-    public Instructor getInstructor() {
-        return inst;
+    public void addInstructor(Instructor instructor) {
+        this.Inst.add(instructor);
     }
-    public void setInstructor(Instructor instructor) {
-        this.inst = instructor;
-    }
-    public Textbook getTextbook() {
-        return tbook;
-    }
-    public void setTextbook(Textbook textbook) {
-        this.tbook= textbook;
+    public void addTextbook(Textbook textbook) {
+        this.tBook.add(textbook);
     }
 
     public void print(){
         System.out.println("Course Name: " + this.cName);
-        System.out.println("Instructor: " + inst.getFName() + " " + inst.getLName());
-        System.out.println("Textbook: " + tbook.getTitle() + " by " + tbook.getAuthor());
+
+        System.out.println("Instructor:" );
+        for (Instructor inst : this.Inst) {
+            System.out.println("  " +  inst.getFName() + " " + inst.getLName() + " Office Number: " + inst.getOfficeNo());
+        }
+        System.out.println("Textbook:" );
+        for (Textbook tBook : this.tBook) {
+            System.out.println("  " +  tBook.getTitle() + " by " + tBook.getAuthor() + " Publisher: " + tBook.getPublisher());
+        }
     }
 }
+
+
+
+
+// FOR PERSONAL REFERENCE
+
+//    private Instructor inst;            // can string be used?
+//    private Textbook tbook;
+//        this.inst = instructor;
+//        this.tbook = textbook;
+
+//    public Instructor getInstructor() {
+//        return inst;
+//    }
+//    public void setInstructor(Instructor instructor) {
+//        this.inst = instructor;
+//    }
+//    public Textbook getTextbook() {
+//        return tbook;
+//    }
+//    public void setTextbook(Textbook textbook) {
+//        this.tbook= textbook;
+//    }
+
+//        System.out.println("Instructor: " + inst.getFName() + " " + inst.getLName());
+//        System.out.println("Textbook: " + tbook.getTitle() + " by " + tbook.getAuthor());
